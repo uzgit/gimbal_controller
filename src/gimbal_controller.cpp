5c3ba972d3308c0c91b252d549d6e954e579025a
Joshua <jspri17@gmail.com>
2020-02-03 03:59:27 +0000

    Started on publisher/subscriber.

--- src/gimbal_controller.cpp
+++ src/gimbal_controller.cpp
@@ -1,19 +1,64 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include <iostream>
+#include <std_msgs/String.h>
+#include <std_msgs/Float64.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <gazebo_msgs/LinkState.h>
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+ros::Publisher publisher;
+
+void callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+	int num_markers = msg->markers.size();
+	double x, y, z;
+	double rotation_x, rotation_y, rotation_z, rotation_w;
+	ROS_INFO("Detected %d markers.", num_markers);
+
+	for(int i = 0; i < 1; i ++)
+	{
+		int id = msg->markers[i].id;
+
+		x = msg->markers[i].pose.position.x;
+		y = msg->markers[i].pose.position.y;
+		z = msg->markers[i].pose.position.z;
+
+		rotation_x = msg->markers[i].pose.orientation.x;
+		rotation_y = msg->markers[i].pose.orientation.y;
+		rotation_z = msg->markers[i].pose.orientation.z;
+		rotation_w = msg->markers[i].pose.orientation.w;
+
+		ROS_INFO("\n\tID: %d", id);
+		ROS_INFO("\n\tPose: (%0.3f, %0.3f, %0.3f)\n\tRotation:(%0.3f, %0.3f, %0.3f, %0.3f)",
+			x,
+			y,
+			z,
+			rotation_x,
+			rotation_y,
+			rotation_z,
+			rotation_w);
+	}
+
+	gazebo_msgs::LinkState link_state_msg;
+	link_state_msg.link_name = "camera_link";
+	link_state_msg.pose.orientation.x = rotation_x;
+	link_state_msg.pose.orientation.y = rotation_y;
+	link_state_msg.pose.orientation.z = rotation_z;
+	link_state_msg.pose.orientation.w = rotation_w;
+//	link_state_msg.reference_frame = "parent";
+	publisher.publish(link_state_msg);
+	
 }
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello, ROS World!" << std::endl;
+	ROS_INFO("Started gimbal_controller node.");
 
-	ros::init(argc, argv, "listener");
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+	ros::init(argc, argv, "gimbal_controller");
+	ros::NodeHandle node_handle;
+	ros::Subscriber subscriber = node_handle.subscribe("/whycon_ros/visual", 1000, callback);
+	//ros::Publisher
+	publisher  = node_handle.advertise<gazebo_msgs::LinkState>("test", 1000);
 	ros::spin();
 
 	//read marker array topic from whycon
