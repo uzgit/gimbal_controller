16b02806c04777086b351e6e19dd69266328c82c
Joshua <jspri17@gmail.com>
2020-02-03 02:11:28 +0000

    just added some pseudo code

--- src/gimbal_controller.cpp
+++ src/gimbal_controller.cpp
@@ -16,5 +16,23 @@ int main(int argc, char **argv)
 	ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 	ros::spin();
 
+	//read marker array topic from whycon
+	//determine if any of the markers are 2-bit whycon marker with id 1 or 2
+	//	id checking may be necessary to determine yaw with rotationally
+	//	symmetric ids 1 and 2
+	//determine if any of the markers are apriltags with id 489 (420 + 69 nice)
+	//if time since marker detection exceeds some time t_epsilon
+	//	set gimbal rotation back to (0, 0)
+	//	OR
+	//	search the field of view for more markers
+	//else
+	//	determine optimal viewing angle
+	//		if only a single marker is visible
+	//			put it to the center of the view
+	//		if both markers are visible
+	//			put the center of the view somewhere on a line between the markers,
+	//			closer to the one with the larger area
+	//	Maintain 2-d PID controller to attempt to set the target point in the middle
+
 	return 0;
 }
