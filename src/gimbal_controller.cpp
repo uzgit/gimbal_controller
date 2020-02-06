2579157a6fdaf493bab19dae0f43978f92b43cfa
Josh <jspri17@gmail.com>
2020-02-06 12:11:01 +0000

    Added an idle state.

--- src/gimbal_controller.cpp
+++ src/gimbal_controller.cpp
@@ -28,11 +28,16 @@
 //			closer to the one with the larger area
 //	Maintain 2-d PID controller to attempt to set the target point in the middle
 
+double idle_setpoint_x = 0.0;
+double idle_setpoint_y = 0.2;
+
 std_msgs::Float64 setpoint_x;
 std_msgs::Float64 setpoint_y;
 
 ros::Publisher setpoint_publisher_x;
 ros::Publisher setpoint_publisher_y;
+
+ros::Time last_detection_time(0);
 
 void visual_callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 {
@@ -54,24 +59,26 @@ void visual_callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 		rotation_z = msg->markers[i].pose.orientation.z;
 		rotation_w = msg->markers[i].pose.orientation.w;
 
-		ROS_INFO("\n\tID: %d", id);
-		ROS_INFO("\n\tPose: (%0.3f, %0.3f, %0.3f)\n\tRotation:(%0.3f, %0.3f, %0.3f, %0.3f)",
-			x,
-			y,
-			z,
-			rotation_x,
-			rotation_y,
-			rotation_z,
-			rotation_w);
+//		ROS_INFO("\n\tID: %d", id);
+//		ROS_INFO("\n\tPose: (%0.3f, %0.3f, %0.3f)\n\tRotation:(%0.3f, %0.3f, %0.3f, %0.3f)",
+//			x,
+//			y,
+//			z,
+//			rotation_x,
+//			rotation_y,
+//			rotation_z,
+//			rotation_w);
 	}
 	
-	setpoint_x.data =  - atan(x / z);
-	setpoint_y.data =  atan(y / z);
+	setpoint_x.data = - atan(x / z);
+	setpoint_y.data = atan(y / z);
 
 	setpoint_publisher_x.publish(setpoint_x);
 	setpoint_publisher_y.publish(setpoint_y);
 
-	ROS_INFO("setpoint = (%f, %f)", setpoint_x.data, setpoint_y.data);
+	last_detection_time = ros::Time::now();
+
+//	ROS_INFO("setpoint = (%f, %f)", setpoint_x.data, setpoint_y.data);
 }
 
 int main(int argc, char **argv)
@@ -88,7 +95,32 @@ int main(int argc, char **argv)
 	setpoint_publisher_x = node_handle.advertise<std_msgs::Float64>("/iris/camera/x/setpoint", 1000);
 	setpoint_publisher_y = node_handle.advertise<std_msgs::Float64>("/iris/camera/y/setpoint", 1000);
 
-	ros::spin();
+	// initialize gimbal position to forward level
+	setpoint_x.data = 0;
+	setpoint_y.data = 0;
+	setpoint_publisher_x.publish(setpoint_x);
+	setpoint_publisher_y.publish(setpoint_y);
+
+	ros::Duration detection_fail_timeout(2.0);
+	ros::Rate loop_rate(70);
+
+	while( ros::ok() )
+	{
+		ros::spinOnce();
+
+		if( ros::Time::now() - last_detection_time > detection_fail_timeout)
+		{
+			// set the gimbal to a favorable attitude
+			setpoint_x.data = 0;
+			setpoint_y.data = 0.1;
+			
+			// publish the message
+			setpoint_publisher_x.publish(idle_setpoint_x);
+			setpoint_publisher_y.publish(idle_setpoint_y);
+		}
+
+		loop_rate.sleep();
+	}
 
 	return 0;
 }
