fd814808d9665e601d6bb509e7a7c1743e74a762
Josh <jspri17@gmail.com>
2020-02-28 19:06:43 +0000

    Removed some old scratch work.

--- src/gimbal_controller.cpp
+++ src/gimbal_controller.cpp
@@ -15,24 +15,6 @@
 #include <cmath>
 #include <ctime>
 
-//read marker array topic from whycon
-//determine if any of the markers are 2-bit whycon marker with id 1 or 2
-//	id checking may be necessary to determine yaw with rotationally
-//	symmetric ids 1 and 2
-//determine if any of the markers are apriltags with id 489 (420 + 69 nice)
-//if time since marker detection exceeds some time t_epsilon
-//	set gimbal rotation back to (0, 0)
-//	OR
-//	search the field of view for more markers
-//else
-//	determine optimal viewing angle
-//		if only a single marker is visible
-//			put it to the center of the view
-//		if both markers are visible
-//			put the center of the view somewhere on a line between the markers,
-//			closer to the one with the larger area
-//	Maintain 2-d PID controller to attempt to set the target point in the middle
-
 int landing_pad_id[2] = {1, 2};
 
 double idle_setpoint_x = 0.0;
@@ -68,14 +50,11 @@ void visual_callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 	int num_markers = msg->markers.size();
 	double x, y, z;
 	double rotation_x, rotation_y, rotation_z, rotation_w;
-//	ROS_INFO("Detected %d marker(s).", num_markers);
 
 	int i = 0;
 	while( i < num_markers && ! detection )
 	{
 		int id = msg->markers[i].id;
-
-//		ROS_INFO("id: %d", id);
 
 		x = msg->markers[i].pose.position.x;
 		y = msg->markers[i].pose.position.y;
@@ -86,16 +65,6 @@ void visual_callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 		rotation_z = msg->markers[i].pose.orientation.z;
 		rotation_w = msg->markers[i].pose.orientation.w;
 
-//		ROS_INFO("\n\tID: %d", id);
-//		ROS_INFO("\n\tPose: (%0.3f, %0.3f, %0.3f)\n\tRotation:(%0.3f, %0.3f, %0.3f, %0.3f)",
-//			x,
-//			y,
-//			z,
-//			rotation_x,
-//			rotation_y,
-//			rotation_z,
-//			rotation_w);
-//
 		int ii = 0;
 		while( ii < 2 && landing_pad_id[ii] != id )
 		{
@@ -128,8 +97,6 @@ void visual_callback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 		idle_state_msg.data = false;
 
 		last_detection_time = ros::Time::now();
-
-	//	ROS_INFO("setpoint = (%f, %f)", setpoint_x.data, setpoint_y.data);
 	}
 }
 
@@ -191,7 +158,6 @@ int main(int argc, char **argv)
 		camera_transform_stamped.transform.translation.y = 0;
 		camera_transform_stamped.transform.translation.z = 0;
 		tf2::Quaternion camera_rotation;
-//		camera_rotation.setRPY(0, setpoint_y.data, setpoint_x.data);
 		camera_rotation.setRPY(0, gimbal_y_position, gimbal_x_position);
 
 		camera_transform_stamped.transform.rotation.w = camera_rotation.w();
