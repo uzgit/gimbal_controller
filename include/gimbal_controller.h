9e2d48d6737f219d300f9d1e10677b92584a2af9
Josh <jspri17@gmail.com>
2020-07-23 12:19:06 +0100

    Cleaned up the code a bit, added initial pwm->angle function, rewrote idle mode, manual gimbal control available in all modes except for 'GUIDED'.

--- include/gimbal_controller.h
+++ include/gimbal_controller.h
@@ -1,11 +1,14 @@
 #ifndef GIMBAL_CONTROLLER_H
 #define GIMBAL_CONTROLLER_H
 
+#include <cstdlib>
 #include "ros/ros.h"
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Pose.h>
 #include <gazebo_msgs/LinkState.h>
 #include <gazebo_msgs/LinkStates.h>
+#include <mavros_msgs/OverrideRCIn.h>
+#include <mavros_msgs/State.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/Imu.h>
 #include <std_msgs/Bool.h>
@@ -16,6 +19,7 @@
 #include <tf2/transform_datatypes.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <whycon_ros/MarkerArray.h>
 
 #include <apriltag_ros/AprilTagDetectionArray.h>
 
@@ -43,13 +47,53 @@ double body_pitch;
 double body_roll;
 double body_yaw;
 
+std_msgs::Float64 landing_pad_pixel_position_x;
+std_msgs::Float64 landing_pad_pixel_position_y;
+std_msgs::Float64 camera_pid_control_effort_x;
+std_msgs::Float64 camera_pid_control_effort_y;
+std_msgs::Float64 camera_pid_setpoint_x;
+std_msgs::Float64 camera_pid_setpoint_y;
 std_msgs::Float64 setpoint_x;
 std_msgs::Float64 setpoint_y;
 std_msgs::Bool idle_state_msg;
-double setpoint_scalar = 0.5;
+std_msgs::Bool camera_pid_x_enable;
+std_msgs::Bool camera_pid_y_enable;
+double setpoint_scalar = 0.75;
+
+// these may actually change through the course of execution so they need to be actual variables
+double camera_pixel_height = 640;
+double camera_pixel_width  = 480;
+
+int tilt_idle_pwm = 1350;
+int pan_idle_pwm  = 1500;
+int tilt_pwm = tilt_idle_pwm;
+int pan_pwm  = pan_idle_pwm;
+
+#define PWM_MIN 1100
+#define PWM_MAX 1900
+#define TILT_CHANNEL 7
+#define PAN_CHANNEL  8
+//int pan_channel  = 6;
+//int tilt_channel = 8;
+double pan_max  =  0.523599; // -30 deg
+double pan_min  = -0.523599; //  30 deg
+double tilt_max =  0.174533; //  10 deg
+double tilt_min = -1.5708;   // -90 deg
+
+double pan_angle = 0;
+double tilt_angle = 0;
+
+mavros_msgs::OverrideRCIn override_rc_in_message;
+mavros_msgs::State state;
 
 ros::Publisher setpoint_publisher_x;
 ros::Publisher setpoint_publisher_y;
+ros::Publisher landing_pad_pixel_position_x_publisher;
+ros::Publisher landing_pad_pixel_position_y_publisher;
+ros::Publisher camera_pid_enable_x_publisher;
+ros::Publisher camera_pid_enable_y_publisher;
+ros::Publisher camera_pid_setpoint_x_publisher;
+ros::Publisher camera_pid_setpoint_y_publisher;
 ros::Publisher idle_state_publisher;
 ros::Publisher landing_pad_camera_pose_publisher;
 ros::Publisher whycon_camera_pose_publisher;
@@ -57,6 +101,11 @@ ros::Publisher apriltag_camera_pose_publisher;
 ros::Publisher yaw_displacement_publisher;
 ros::Publisher landing_pad_relative_pose_publisher;
 ros::Publisher vo_publisher;
+ros::Publisher override_rc_in_publisher;
+ros::Publisher camera_pid_x_enable_publisher;
+ros::Publisher camera_pid_y_enable_publisher;
+ros::Publisher camera_pan_publisher;
+ros::Publisher camera_tilt_publisher;
 
 ros::Duration detection_timeout(2);
 ros::Time last_detection_time(0);
@@ -74,7 +123,7 @@ geometry_msgs::PoseStamped landing_pad_relative_pose;
 void gimbal_x_position_callback( const std_msgs::Float64::ConstPtr );
 void gimbal_y_position_callback( const std_msgs::Float64::ConstPtr );
 void apriltag_visual_callback( const apriltag_ros::AprilTagDetectionArray::ConstPtr& );
-void whycon_visual_callback( const visualization_msgs::MarkerArray::ConstPtr& );
+void whycon_visual_callback( const whycon_ros::MarkerArray::ConstPtr& );
 
 geometry_msgs::PoseStamped straighten_pose( const geometry_msgs::PoseStamped& );
 
